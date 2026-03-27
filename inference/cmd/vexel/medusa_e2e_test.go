db73164d34bcc5dc941ba7229e195d0065ebaf95
Keith Ballinger <keithba@gmail.com>
2026-03-27 16:51:10 -0700

    test(medusa): add E2E smoke test for MedusaScheduler creation

---
+++ inference/cmd/vexel/medusa_e2e_test.go
@@ -0,0 +1,38 @@
+//go:build metal && darwin && cgo
+
+package main
+
+import (
+	"os"
+	"testing"
+
+	"vexel/inference/scheduler"
+)
+
+func TestMedusaSchedulerCreation(t *testing.T) {
+	modelPath := os.Getenv("VEXEL_TEST_MODEL")
+	if modelPath == "" {
+		t.Skip("VEXEL_TEST_MODEL not set; skipping E2E test")
+	}
+
+	rt, tok, _, err := initModel(modelPath, 128, false)
+	if err != nil {
+		t.Fatalf("initModel failed: %v", err)
+	}
+
+	cfg := scheduler.Config{
+		MaxBatchSize: 1,
+		MaxSequences: 1,
+		MaxTokens:    32,
+	}
+	medusaCfg := scheduler.DefaultMedusaConfig()
+
+	ms, err := scheduler.NewMedusaScheduler(rt, tok, cfg, medusaCfg)
+	if err != nil {
+		t.Fatalf("NewMedusaScheduler failed: %v", err)
+	}
+
+	if ms.BaseScheduler() == nil {
+		t.Fatal("BaseScheduler() returned nil")
+	}
+}
